fed08f190050937dbdf9c283eb91c21da0a43813
triny <trinylm3@gmail.com>
2023-07-16 19:45:11 -0600

    first commit

---
+++ app/src/main/java/com/itsz/apprepo/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.itsz.apprepo
+
+import android.app.usage.UsageEvents.Event
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+    fun loging(even : Event){
+
+    }
+}
